5da6f3176754b84c2efa3589f9b4a6991bbb22a3
Artem Martiyanov <artem-martiyanov@mail.ru>
2023-07-22 13:05:10 +0300

    Adds card filter

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-import {renderCards} from './render-cards.js';
 import './upload-image-form.js';
 import {getData} from './api.js';
 import {showAlert} from './utils.js';
+import {initCardFilter} from './card-filter.js';
 getData()
-  .then((cards) => renderCards(cards))
+  .then((cardsData) => initCardFilter(cardsData))
   .catch((error) => showAlert(error.message));
